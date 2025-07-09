540b2c51b521899866919829669d2fe4576d7b15
Zek Julien <110969316+zekjulien@users.noreply.github.com>
2025-07-09 11:15:15 +0200

    Initial commit

--- src/app/root/app.ts
+++ src/app/root/app.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {DemoBinding} from '../features/demos/binding/binding';
+import {ExoBinding} from '../features/exercices/binding/binding';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    DemoBinding,
+    ExoBinding
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected title = 'tf-exo';
+
 }
